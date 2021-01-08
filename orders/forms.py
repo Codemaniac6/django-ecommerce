e7562a2d19077252b5523995aca8e38d70e01533
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2021-01-08 23:57:29 +0100

    more updates

--- orders/forms.py
+++ orders/forms.py
@@ -3,6 +3,15 @@ from .models import Order
 
 
 class OrderCreateForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(max_length=100, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    address = forms.CharField(max_length=250, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    postal_code = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    city = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
     class Meta:
         model = Order
-        fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
+        fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city'
+
+
