12216f01b1cb752df7e6b08b82c78f996db8873d
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2021-01-08 19:51:27 +0100

    checkout updates

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import OrderItem
+from .models import OrderItem, Order
 from cart.cart import Cart
 from .forms import OrderCreateForm
 
@@ -7,21 +7,36 @@ from .forms import OrderCreateForm
 def order_create(request):
     cart = Cart(request)
     if request.method == "POST":
-        form = OrderCreateForm(request.POST)
-        if form.is_valid():
-            order = form.save()
+        order = Order(request.POST)
+        if order:
             for product in cart:
                 OrderItem.objects.create(order=order,
                                          item=product['item'],
                                          price=product['price'],
                                          quantity=product['quantity'])
-                # clears the cart.
                 cart.clear()
                 return render(request, 'orders/order/created.html',
-                              {'order': order})
+                              {'order': order,
+                               'cart': cart})
     else:
-        form = OrderCreateForm()
+        order = Order()
         return render(request, 'checkout-page.html',
-                      {'cart': cart,
-                       'form': form})
-
+                      {'order': order,
+                       'cart': cart})
+        # form = OrderCreateForm(request.POST)
+        # if form.is_valid():
+        #     order = form.save()
+        #     for product in cart:
+        #         OrderItem.objects.create(order=order,
+        #                                  item=product['item'],
+        #                                  price=product['price'],
+        #                                  quantity=product['quantity'])
+        #         # clears the cart.
+    #             cart.clear()
+    #             return render(request, 'orders/order/created.html',
+    #                           {'order': order})
+    # else:
+    #     form = OrderCreateForm()
+    #     return render(request, 'checkout-page.html',
+    #                   {'cart': cart,
+    #                    'form': form})
