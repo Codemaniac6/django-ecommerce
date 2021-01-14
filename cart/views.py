4b803ed1f48c254cae6d31cfd6e7f5bb4349e9ba
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2021-01-14 02:09:50 +0100

    added coupon to cart and created template

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from shop.models import Item
+from coupons.forms import CouponApplyForm
 from .cart import Cart
 from .forms import CartAddItemForm
 
@@ -33,6 +34,9 @@ def cart_detail(request):
             'quantity': product['quantity'],
             'override': True
         })
+    coupon_apply_form = CouponApplyForm()
+
     return render(request,
                   'cart/detail.html',
-                  {'cart': cart})
+                  {'cart': cart,
+                   'coupon_apply_form': coupon_apply_form})
