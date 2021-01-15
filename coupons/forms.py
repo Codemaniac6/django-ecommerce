2eec081ae5fd8f5d8740641817f4d9899e58ab56
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2021-01-15 22:42:46 +0100

    coupon updates

--- coupons/forms.py
+++ coupons/forms.py
@@ -1,13 +1,7 @@
 from django import forms
-from .models import Coupon
 
 
 class CouponApplyForm(forms.Form):
     code = forms.CharField(widget=forms.TextInput(
             attrs={'class': 'form-control'}
         ))
-        # widget = {
-        #     'code': forms.TextInput(
-        #         attrs={'class': 'form-control'}
-        #     )
-        # }
