51a3c6244f647126451c353a87a1e128a2b643c5
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2021-01-16 21:42:11 +0100

    added coupon to order model

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from shop.models import Item
 from django.core.validators import MinValueValidator, MaxValueValidator
+from decimal import Decimal
+from coupons.models import Coupon
 
 
 # Create your models here.
@@ -16,6 +18,12 @@ class Order(models.Model):
     updated = models.DateField(auto_now_add=True)
     paid = models.BooleanField(default=False)
     braintree_id = models.CharField(max_length=150, blank=True)
+    coupon = models.ForeignKey(Coupon,
+                               related_name='orders',
+                               null=True,
+                               on_delete=models.SET_NULL)
+    discount = models.IntegerField(default=0,
+                                   validators=[MinValueValidator(0), MaxValueValidator(100)])
 
     class Meta:
         ordering = ('-created',)
@@ -24,7 +32,8 @@ class Order(models.Model):
         return f'Order {self.id}'
 
     def get_total_cost(self):
-        return sum(product.get_cost() for product in self.products.all())
+        total_cost = sum(product.get_cost() for product in self.products.all())
+        return total_cost - total_cost * (self.discount / Decimal(100))
 
 
 class OrderItem(models.Model):
