80309cfe116568c5f2598ca3b761025658fb6e0f
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2021-01-28 23:56:43 +0100

    added discounted price to model

--- shop/models.py
+++ shop/models.py
@@ -69,6 +69,7 @@ class Item(models.Model):
                               blank=True)
 
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
