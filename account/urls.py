d065a70e13838d8b3b67b04af39fe0093d16bb7d
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2021-02-01 00:49:52 +0100

    added password reset views

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 from . import views
 
 app_name = 'account'
@@ -8,4 +9,11 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('password_change/',
+         auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('account:password_change_done')),
+         name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/confirm', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset/done', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
 ]
