9e16d21dc1395cb89883ee723235991442d9f68d
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2021-02-01 22:15:32 +0100

    completed registration template and views

--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 
 def user_login(request):
@@ -24,3 +24,16 @@ def user_login(request):
         form = LoginForm()
 
     return render(request, 'account/login.html', {'form': form})
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'registration_done.html', {'form': form})
+    else:
+        form = RegistrationForm
+    return render(request, 'registration.html', {'form': form})
